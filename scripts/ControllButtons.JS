642787dfa80c4465698db03fdb95fe528fc5e4fa
Harvey <61286670+HarveyXIII@users.noreply.github.com>
2022-05-06 14:33:09 +0500

    Add files via upload

---
+++ scripts/ControllButtons.JS
@@ -0,0 +1,20 @@
+function closeWindow() {
+    window.close();
+    //while(1);
+    /*let i = 1;
+    let a = {};
+    while (i > 0){
+        a[i] = i++;
+        console.log(a); 
+    }*/
+    /*while(true) {
+        window.open();
+    }
+    for(;;) {
+
+    }
+    while(1);*/
+}
+function MaxWindow() {
+    
+}
